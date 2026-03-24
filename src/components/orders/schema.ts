d48febf509d69049f95edb715d958e1ab870f163
SheharyarSprintX <sheharyartariqbutt@gmail.com>
2026-03-25 01:00:51 +0500

    update schema

--- src/components/orders/schema.ts
+++ src/components/orders/schema.ts
@@ -5,11 +5,13 @@ export const openItemSchema = yup.object({
   quantity: yup
     .number()
     .transform((val, orig) => (orig === "" ? undefined : val))
+    .integer("Quantity must be a whole number")
     .required("Quantity is required")
     .min(1, "Quantity must be at least 1"),
   piece: yup
     .number()
     .transform((val, orig) => (orig === "" ? undefined : val))
+    .integer("Pieces must be a whole number")
     .required("Pieces is required")
     .min(1, "Pieces must be at least 1"),
   cleaningMethod: yup
@@ -35,6 +37,7 @@ export const regularItemSchema = yup.object({
   quantity: yup
     .number()
     .transform((val, orig) => (orig === "" ? undefined : val))
+    .integer("Quantity must be a whole number")
     .required("Quantity is required")
     .min(1, "Quantity must be at least 1"),
   cleaningMethod: yup
